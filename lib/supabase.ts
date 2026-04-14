d6e4e62d811db0bd98d4d27e032792236f8068b1
jeckajeckovich <terekhin.evgeny.e@gmail.com>
2026-04-14 21:02:07 +0200

    hardcode supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://hrxaxnfvroniqrjmqrn.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyeGF4bmZydm9ybmlxcmptcXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzQ2NjMsImV4cCI6MjA5MTY1MDY2M30.tOcG7XWZTSI29lNlL8eehG7A231AvA_0C4inh39tmt8";
+const supabaseAnonKey = "ТВОЙ_ANON_JWT_КЛЮЧ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
